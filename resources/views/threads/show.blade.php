416ccd80a0f2067ab3b4038e97f74b756052cb61
Simarx <simarx.potasio@resultate.com.br>
2017-05-26 18:14:05 -0300

    Lesson 3. Create Replys tests and show it on thread view with user name and reply time.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,6 +22,33 @@
 
     </div>
 
+    <div class="row">
+
+        <div class="col-md-8 col-md-offset-2">
+
+            @foreach($thread->replies as $reply)
+
+                <div class="panel panel-default">
+
+                    <div class="panel-heading"> 
+                        <a href=""> 
+                            {{ $reply->owner->name }}
+                        </a> 
+                        said {{ $reply->created_at->diffForHumans() }}
+                    </div>
+
+                    <div class="panel-body">                    
+                        {{ $reply->body }}
+                    </div>
+
+                </div>
+
+            @endforeach
+
+        </div>
+
+    </div>
+
 </div>
 
 @endsection
