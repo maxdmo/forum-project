b0c26cc96a575f53d924ea82601a24c234cbcb86
Simarx Potasio <simarx.potasio@resultate.com.br>
2017-05-29 18:09:32 -0300

    Testing commit.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Teste
                 </div>
 
                 <div class="links">
